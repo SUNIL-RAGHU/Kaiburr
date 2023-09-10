52aa61276cab9ca0fb8b671226284d6239c2472d
sunil <sunilraghu199@gmail.com>
2023-09-10 22:55:45 +0530

    :sparkles: Creating Mvc pattern

---
+++ src/main/java/com/sunil/Kaiburr/Kaiburr/KaiburrApplication.java
@@ -0,0 +1,12 @@
+package com.sunil.Kaiburr.Kaiburr;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaiburrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaiburrApplication.class, args);
+	}
+
+}
