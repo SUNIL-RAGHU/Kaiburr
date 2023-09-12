25ec6a6c803267911609e0747c7ed0e1eb9b5e80
sunil <sunilraghu199@gmail.com>
2023-09-12 22:41:45 +0530

    :whale: work about Docker

--- src/main/java/com/sunil/Kaiburr/Kaiburr/KaiburrApplication.java
+++ src/main/java/com/sunil/Kaiburr/Kaiburr/KaiburrApplication.java
@@ -1,10 +1,11 @@
 package com.sunil.Kaiburr.Kaiburr;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 
 @SpringBootApplication
-
+@EnableMongoRepositories
 
 
 public class KaiburrApplication {
